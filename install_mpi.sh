3b8e49443a10b23fd58c44ba565b598a96c1faaf
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-10 16:10:08 -0400

    Create install_mpi.sh
    Tweaked commands to install into the shared drive. Will need to source correctly on the clients.

---
+++ install_mpi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+sudo yum -y update
+sudo yum -y group install "Development Tools"
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/mnt/software/openmpi/3.1.2
+sudo make
+sudo make all install
+echo "export PATH='$PATH:/mnt/software/openmpi/3.1.2/bin'" >> /users/BW840606/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/mnt/software/openmpi/3.1.2/lib/'" >> /users/BW840606/.bashrc
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz
+source /users/BW840606/.bashrc
