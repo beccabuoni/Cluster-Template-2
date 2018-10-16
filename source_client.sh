fdd44896905a136f6ae46ae512441335d380f16c
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-16 18:05:38 -0400

    Update source_client.sh
    Changed to use root directory.

--- source_client.sh
+++ source_client.sh
@@ -1,3 +1,3 @@
-echo "export PATH='$PATH:/mnt/software/openmpi/3.1.2/bin'" >> /users/BW840606/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/mnt/software/openmpi/3.1.2/lib/'" >> /users/BW840606/.bashrc
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/BW840606/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/BW840606/.bashrc
 source /users/BW840606/.bashrc
