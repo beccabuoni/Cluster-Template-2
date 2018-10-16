23caaeb1da4905cc015ebe17d72d2ac327e78f09
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-16 17:59:41 -0400

    Update install_NFS_client.sh
    Changed to use root directory.

--- install_NFS_client.sh
+++ install_NFS_client.sh
@@ -1,7 +1,7 @@
 sudo yum -y install nfs-utils
-sudo mkdir -p /mnt/software
-sudo mkdir -p /mnt/scratch
-sudo mount -t nfs 192.168.1.1:/mnt/software /mnt/software
-sudo mount -t nfs 192.168.1.3:/mnt/scratch /mnt/scratch
-sudo echo "192.168.1.1:/mnt/software /mnt  nfs defaults 0 0" | sudo tee -a /etc/fstab
-sudo echo "192.168.1.3:/mnt/scratch /mnt  nfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo mkdir -p /software
+sudo mkdir -p /scratch
+sudo mount -t nfs 192.168.1.1:/software /software
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+sudo echo "192.168.1.1:/software /  nfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "192.168.1.3:/scratch /  nfs defaults 0 0" | sudo tee -a /etc/fstab
