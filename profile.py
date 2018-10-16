b6adab387eadc6a9e4183c7c936084c7d76b8537
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-16 18:04:50 -0400

    Update profile.py
    Changed to use root directory.

--- profile.py
+++ profile.py
@@ -37,7 +37,7 @@ for i in range( params.n ):
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
     node.addService(pg.Execute(shell="sh", command="sleep 5m && sudo -H -u BW840606 bash -c '/local/repository/ssh_setup.sh'"))
-    node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'cp /local/repository/source/* /mnt/scratch/'"))
+    node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'cp /local/repository/source/* /scratch/'"))
     node.routable_control_ip = "true"
   elif i == 1:
     node = request.XenVM("metadata")
