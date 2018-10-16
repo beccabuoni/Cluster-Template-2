943f08796d82ff93c01cddaf414c82002d6eea6e
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-16 18:02:24 -0400

    Update install_NFS_storage.sh
    Changed to use root directory.

--- install_NFS_storage.sh
+++ install_NFS_storage.sh
@@ -1,8 +1,8 @@
 sudo yum -y install nfs-utils
-sudo mkdir /mnt/scratch
-sudo chmod 777 /mnt/scratch
-sudo chown nfsnobody:nfsnobody /mnt/scratch
+sudo mkdir /scratch
+sudo chmod 777 /scratch
+sudo chown nfsnobody:nfsnobody /scratch
 #sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
 #sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
-sudo echo '/mnt/scratch *(rw,sync,no_root_squash)' | sudo tee /etc/exports
+sudo echo '/scratch *(rw,sync,no_root_squash)' | sudo tee /etc/exports
 sudo exportfs -ra
